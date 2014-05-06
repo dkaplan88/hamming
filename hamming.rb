b076945919631a05856ebb3ce5167da4f79092fa
dkaplan <dkaplan@enova.com>
2014-05-06 17:36:24 -0500

    transforming strings into arrays, using zip, custom count method

--- hamming.rb
+++ hamming.rb
@@ -1,10 +1,7 @@
 class Hamming
-  def self.compute(dna_one, dna_two, count = 0)
-    short = dna_one.length <= dna_two.length ? dna_one : dna_two
-    long  = short == dna_one ? dna_two : dna_one
-
-    short.each_char.with_index(1) { |char, i| count += 1 if char != long[i-1] }
-
-    count
+  def self.compute(dna_one, dna_two)
+    dna_one.chars.zip(dna_two.chars).count do |dna_one_char, dna_two_char|
+       dna_one_char && dna_two_char && dna_one_char != dna_two_char
+    end
   end
 end
